85c707a4e44d0f393e17d0522dd9e45da710e3bf
EvelynGMaldonado <evelyn.gmaldonado@gmail.com>
2023-05-04 13:50:19 -0700

    setting up repo

---
+++ src/main/java/em_ims/em_inventorymanagementsoftware/HelloController.java
@@ -0,0 +1,14 @@
+package em_ims.em_inventorymanagementsoftware;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module em_ims.em_inventorymanagementsoftware {
+    requires javafx.controls;
+    requires javafx.fxml;
+//    requires javafx.web;
+    requires javafx.graphics;
+    requires java.sql;
+
+//    requires org.controlsfx.controls;
+//    requires com.dlsc.formsfx;
+//    requires validatorfx;
+//    requires org.kordamp.ikonli.javafx;
+//    requires org.kordamp.bootstrapfx.core;
+//    requires eu.hansolo.tilesfx;
+
+    opens em_ims.em_inventorymanagementsoftware to javafx.fxml;
+    exports em_ims.em_inventorymanagementsoftware;
+}
