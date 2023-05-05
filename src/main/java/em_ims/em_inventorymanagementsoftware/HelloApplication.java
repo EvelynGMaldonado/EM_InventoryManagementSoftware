cfb436c0e3e82d4f218f20f2e5d923e98f3b8241
EvelynGMaldonado <evelyn.gmaldonado@gmail.com>
2023-05-05 10:07:17 -0700

    working on landing page and database connection set up

--- src/main/java/em_ims/em_inventorymanagementsoftware/HelloApplication.java
+++ src/main/java/em_ims/em_inventorymanagementsoftware/HelloApplication.java
@@ -7,16 +7,35 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * @author Evelyn G Morrow.
+ * @version 1.0.
+ * Public class HelloApplication is the primary class of the EM Inventory Management System.
+ * Public class HelloApplication extends the Application class, which is a standard class in Java.
+ */
 public class HelloApplication extends Application {
+
+    /**
+     * Public void start() method is called when the JavaFX application is started.
+     * @param stage of the type Stage is taken.
+     * The visual parts of the JavaFX Application (displayed).
+     * @exception IOException if an input or output error occurred.
+     * @see IOException
+     */
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("landing_page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        //scene.getStylesheets().add(getClass().getResource("startPageDesign.css").toString());
+        stage.setTitle("EM-Inventory Management System");
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * Public static void main() method launches the application and let the developer passing command line parameters if needed.
+     * The launch() method is a static method located in the Application class, and it detects from which class it is called, so it is not necessary to explicitly tell it what class to launch.
+     */
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/em_ims/em_inventorymanagementsoftware/HelloController.java
+++ src/main/java/em_ims/em_inventorymanagementsoftware/HelloController.java
@@ -1,14 +1,107 @@
 package em_ims.em_inventorymanagementsoftware;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.sql.*;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+/**
+ * @author Evelyn G Morrow.
+ * @version 1.0.
+ * Public class HelloController is used to retrieve and display the most up-to-date data on the parts and products tables after the user successfully signs in, as well as to manage some functionality such as delete, search, etc.
+ */
+public class HelloController implements Initializable {
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+
+    /**
+     * Public void initialize() method called to initialize a controller after its root element has been completely processed.
+     * @param url is used to resolve relative paths for the root object. It is null if the url is not known.
+     * @param rb is used to localize the root object, and it is null if the root object is not located.
+     * @exception SQLException if a database error or other errors occur.
+     * @see SQLException
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+
+        //SQL Query - executed in the backend database
+        String partsViewQuery = "SELECT partID, part_name, stock, price_unit FROM parts";
+
+        String productsViewQuery = "SELECT productID, product_name, stock, price_unit FROM products";
+
+//        try {
+//            Statement statement = connectDB.createStatement();
+//            ResultSet queryPartsOutput = statement.executeQuery(partsViewQuery);
+//
+//            while (queryPartsOutput.next()) {
+//
+//                //populate the observableList
+//                partList.add(new PartData(queryPartsOutput.getInt("partID"),
+//                        queryPartsOutput.getString("part_name"),
+//                        queryPartsOutput.getInt("stock"),
+//                        queryPartsOutput.getBigDecimal("price_unit")));
+//            }
+//
+//
+//            //PropertyValueFactory corresponds to the new PartData fields
+//            //the table column is the one we annotate above
+//            parts_tableView_col_partID.setCellValueFactory(new PropertyValueFactory<>("partID"));
+//            parts_tableView_col_partName.setCellValueFactory(new PropertyValueFactory<>("part_name"));
+//            parts_tableView_col_inventoryLevel.setCellValueFactory(new PropertyValueFactory<>("stock"));
+//            parts_tableView_col_priceUnit.setCellValueFactory(new PropertyValueFactory<>("price_unit"));
+//
+//            parts_tableView.setItems(partList);
+//            //closing statement once I am done with the query to avoid crashing!!
+//            statement.close();
+//            queryPartsOutput.close();
+//
+//        } catch(SQLException e) {
+//            Logger.getLogger(HelloController.class.getName()).log(Level.SEVERE, null, e);
+//            e.printStackTrace();
+//            e.getCause();
+//        }
+//        try {
+//            Statement statement = connectDB.createStatement();
+//            ResultSet queryProductsOutput = statement.executeQuery(productsViewQuery);
+//
+//            while (queryProductsOutput.next()) {
+//                productList.add(new ProductData(queryProductsOutput.getInt("productID"),
+//                        queryProductsOutput.getString("product_name"),
+//                        queryProductsOutput.getInt("stock"),
+//                        queryProductsOutput.getBigDecimal("price_unit")));
+//            }
+//
+//            products_tableView_col_productID.setCellValueFactory(new PropertyValueFactory<>("productID"));
+//            products_tableView_col_productName.setCellValueFactory(new PropertyValueFactory<>("product_name"));
+//            products_tableView_col_inventoryLevel.setCellValueFactory(new PropertyValueFactory<>("stock"));
+//            products_tableView_col_priceUnit.setCellValueFactory(new PropertyValueFactory<>("price_unit"));
+//
+//            products_tableView.setItems(productList);
+//
+//        } catch (SQLException e) {
+//            Logger.getLogger(HelloController.class.getName()).log(Level.SEVERE, null, e);
+//            e.printStackTrace();
+//            e.getCause();
+//        }
+
     }
 }
